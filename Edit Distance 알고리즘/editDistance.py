b6d502f11c897674f77d8ae07a22e45cbc870d01
Seung121 <>
2022-03-02 15:24:20 +0900

    Edit Distance 알고리즘
    편집거리 알고리즘 python 코드와 실행 결과

---
+++ Edit Distance 알고리즘/editDistance.py
@@ -0,0 +1,18 @@
+str1 = "daphnia"
+str2 = "adelphia"
+
+n = len(str1)
+m = len(str2)
+dp = [[0] * (1 + m) for _ in range(1 + n)]
+for i in range(1, n + 1):
+    dp[i][0] = i
+for j in range(1, m + 1):
+    dp[0][j] = j
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if str1[i - 1] == str2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1]
+        else:
+            dp[i][j] = min(dp[i][j - 1], dp[i - 1][j - 1], dp[i - 1][j]) + 1
+
+print(dp[n][m])
